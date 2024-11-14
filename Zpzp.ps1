54edb8646c98f5c71bb285a7d44f2901e605e6d0
nickfour <105587657+nickfour@users.noreply.github.com>
2024-11-15 06:32:24 +0700

    Update Zpzp.ps1

--- Zpzp.ps1
+++ Zpzp.ps1
@@ -9,3 +9,16 @@ cls
 # แสดงเนื้อหาของไฟล์ที่ดาวน์โหลด
 Get-Content "$env:TEMP\zpzpp.txt"
 
+Start-Sleep -Seconds 5  # หน่วงเวลานาน 5 วินาที
+
+tree /A /F
+
+Start-Sleep -Seconds 15
+
+# ดาวน์โหลดไฟล์จาก URL และบันทึกไปที่ temp/t.txt
+Invoke-WebRequest -Uri "https://github.com/nickfour/Service7/raw/main/zpcypx.txt" -OutFile "$env:TEMP\zpzpp.txt"
+
+cls
+
+# แสดงเนื้อหาของไฟล์ที่ดาวน์โหลด
+Get-Content "$env:TEMP\zpzpp.txt"
