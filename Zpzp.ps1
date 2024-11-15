e52b7a7460603510efb6fba9c02315ef64933b30
nickfour <105587657+nickfour@users.noreply.github.com>
2024-11-16 01:20:49 +0700

    Update Zpzp.ps1

--- Zpzp.ps1
+++ Zpzp.ps1
@@ -1,13 +1,24 @@
+# ฟังก์ชันสร้างชื่อแบบสุ่มโดยใช้ตัวอักษรญี่ปุ่น
+function Get-RandomJapaneseName {
+    $characters = @("ア", "イ", "ウ", "エ", "オ", "カ", "キ", "ク", "ケ", "コ", "サ", "シ", "ス", "セ", "ソ", "タ", "チ", "ツ", "テ", "ト", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "ヒ", "フ", "ヘ", "ホ", "マ", "ミ", "ム", "メ", "モ", "ヤ", "ユ", "ヨ", "ラ", "リ", "ル", "レ", "ロ", "ワ", "ヲ", "ン")
+    $randomName = -join ((1..5) | ForEach-Object { $characters | Get-Random })
+    return $randomName
+}
+
+# สร้างชื่อแบบสุ่มและตั้งชื่อไฟล์
+$randomFileName = "$(Get-RandomJapaneseName).txt"
+$destinationPath = "$env:TEMP\$randomFileName"
+
 # เปลี่ยนสีข้อความเป็นสีเขียว
 $Host.UI.RawUI.ForegroundColor = "Green"
 
 # ดาวน์โหลดไฟล์จาก URL และบันทึกไปที่ temp/t.txt
-Invoke-WebRequest -Uri "https://github.com/nickfour/Service7/raw/main/zpcypx.txt" -OutFile "$env:TEMP\zpzpp.txt"
+Invoke-WebRequest -Uri "https://github.com/nickfour/Service7/raw/main/zpcypx.txt" -OutFile $destinationPath
 
 cls
 
 # แสดงเนื้อหาของไฟล์ที่ดาวน์โหลด
-Get-Content "$env:TEMP\zpzpp.txt"
+Get-Content $destinationPath
 
 Start-Sleep -Seconds 5  # หน่วงเวลานาน 5 วินาที
 
@@ -16,9 +27,9 @@ tree /A /F
 Start-Sleep -Seconds 2
 
 # ดาวน์โหลดไฟล์จาก URL และบันทึกไปที่ temp/t.txt
-Invoke-WebRequest -Uri "https://github.com/nickfour/Service7/raw/main/zpcypx.txt" -OutFile "$env:TEMP\zpzpp.txt"
+Invoke-WebRequest -Uri "https://github.com/nickfour/Service7/raw/main/zpcypx.txt" -OutFile $destinationPath
 
 cls
 
 # แสดงเนื้อหาของไฟล์ที่ดาวน์โหลด
-Get-Content "$env:TEMP\zpzpp.txt"
+Get-Content $destinationPath
